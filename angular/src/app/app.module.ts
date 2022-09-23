e76a751691d6a059ed1106841ec6fe9582a744a8
trungnguyendn91 <trungnguyendn91@gmail.com>
2022-09-23 16:29:09 +0700

    ss3

---
+++ angular/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FontSizeEditorComponent } from './font-size-editor/font-size-editor.component';
+import {FormsModule} from '@angular/forms';
+import { AngularShowPetInfoComponent } from './angular-show-pet-info/angular-show-pet-info.component';
+import { AngularProductListComponent } from './angular-product-list/angular-product-list.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { ColorComponent } from './color/color.component';
+import { ArticleComponent } from './article/article.component';
+import { LikeComponent } from './like/like.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FontSizeEditorComponent,
+    AngularShowPetInfoComponent,
+    AngularProductListComponent,
+    CalculatorComponent,
+    ColorComponent,
+    ArticleComponent,
+    LikeComponent,
+    NavbarComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
